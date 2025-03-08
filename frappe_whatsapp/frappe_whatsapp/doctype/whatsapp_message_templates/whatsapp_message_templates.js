13381649c917e11994cb022d8be6f017e3fd57da
shliew97 <sh.liew97@gmail.com>
2025-03-08 09:06:49 +0000

    feat: filter message templates

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message_templates/whatsapp_message_templates.js
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message_templates/whatsapp_message_templates.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2025, Shridhar Patil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("WhatsApp Message Templates", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("WhatsApp Message Templates", {
+    refresh(frm) {
+        if (!frm.is_new()) {
+            frm.set_query("reply_whatsapp_template_if_button_clicked", "whatsapp_message_template_buttons", () => {
+                return {
+                    filters: {
+                        name: ["!=", frm.doc.name]
+                    }
+                }
+            });
+        }
+    },
+});
