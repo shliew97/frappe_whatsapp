8eeee518615dcd9f792fd83fde0cc033c0572918
shliew97 <sh.liew97@gmail.com>
2025-03-12 01:17:08 +0000

    feat: assign lead to a flow

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -367,9 +367,11 @@ def handle_text_message(message, whatsapp_id, customer_name):
         else:
             send_message(crm_lead_doc, whatsapp_id, INSUFFICIENT_VOUCHER_COUNT_MESSAGE)
     else:
-        text_auto_replies = frappe.db.get_all("Text Auto Reply", filters={"disabled": 0, "keyword": message}, pluck="whatsapp_interaction_message_templates")
+        text_auto_replies = frappe.db.get_all("Text Auto Reply", filters={"disabled": 0, "keyword": message}, fields=["whatsapp_message_templates", "whatsapp_interaction_message_templates"])
         if text_auto_replies:
-            enqueue(method=send_interaction_with_delay, crm_lead_doc=crm_lead_doc, whatsapp_id=whatsapp_id, whatsapp_interaction_message_template=text_auto_replies[0], queue="short", is_async=True)
+            crm_lead_doc.whatsapp_message_templates = text_auto_replies[0].whatsapp_message_templates
+            crm_lead_doc.save(ignore_permissions=True)
+            enqueue(method=send_interaction_with_delay, crm_lead_doc=crm_lead_doc, whatsapp_id=whatsapp_id, whatsapp_interaction_message_template=text_auto_replies[0].whatsapp_interaction_message_templates, queue="short", is_async=True)
 
 def handle_interactive_message(interactive_id, whatsapp_id, customer_name):
     crm_lead_doc = get_crm_lead(whatsapp_id, customer_name)
