e4bc025360f53ddf1101a8142bce9547d2108576
shliew97 <sh.liew97@gmail.com>
2025-04-27 01:18:53 +0000

    fix: permission query for whatsapp tempalte

--- frappe_whatsapp/permissions/whatsapp_templates.py
+++ frappe_whatsapp/permissions/whatsapp_templates.py
@@ -12,6 +12,6 @@ def whatsapp_template_query(user):
 
     if "Booking Centre" in user_roles:
         users = get_users_with_role("Booking Centre")
-        return """`tabWhatsApp Templates`.owner IN ({0}) """.format(",".join(frappe.db.escape(user) for user in users[0]))
+        return """`tabWhatsApp Templates`.owner IN ({0}) """.format(",".join(frappe.db.escape(user) for user in users))
 
     return """`tabWhatsApp Templates`.owner = "{0}" """.format(user)
