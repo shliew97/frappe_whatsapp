c4f49f5d15c972b1c0118e87bfee9add212c2150
shliew97 <sh.liew97@gmail.com>
2025-11-12 09:10:08 +0000

    f

--- frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
+++ frappe_whatsapp/frappe_whatsapp/doctype/whatsapp_message/whatsapp_message.py
@@ -88,6 +88,7 @@ PDPA_BUTTON = [
         }
     }
 ]
+PDPA_ACCEPTED_REPLY = "You're now registered as a SOMA Wellness Member 🌿"
 
 class WhatsAppMessage(Document):
     """Send whats app messages."""
@@ -736,7 +737,7 @@ def handle_interactive_message(interactive_id, whatsapp_id, customer_name, crm_l
 
     if interactive_id == "agree-pdpa":
         frappe.flags.agree_pdpa = True
-        enqueue(method=send_message_with_delay, crm_lead_doc=crm_lead_doc, whatsapp_id=whatsapp_id, text="You're now registered as a SOMA Wellness Member 🌿", queue="short", is_async=True)
+        enqueue(method=send_message_with_delay, crm_lead_doc=crm_lead_doc, whatsapp_id=whatsapp_id, text=PDPA_ACCEPTED_REPLY, queue="short", is_async=True)
 
     whatsapp_interaction_message_template_buttons = frappe.db.get_all("WhatsApp Interaction Message Template Buttons", filters={"reply_id": interactive_id}, fields=["*"])
 
